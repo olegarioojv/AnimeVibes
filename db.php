767ab23d7454c3a055373ccd7b759cf40fa3e04c
João Victor <168527020+olegarioojv@users.noreply.github.com>
2024-10-21 18:38:45 -0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-  $db_name = "moviestar";
+  $db_name = "animevibes";
   $db_host = "localhost";
   $db_user = "root";
-  $db_pass = "123456";
+  $db_pass = "";
 
   $conn = new PDO("mysql:dbname=". $db_name .";host=". $db_host, $db_user, $db_pass);
 
